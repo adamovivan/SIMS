83bc09a5ab1b99f205f20734b7463d2fe988a19c
motomaniac96 <tomic.nikola.uns@gmail.com>
2017-07-07 00:18:08 +0200

    problems

---
+++ TourGuide/src/state/GuideMainView.java
@@ -0,0 +1,29 @@
+/***********************************************************************
+ * Module:  GuideMainView.java
+ * Author:  Nikola
+ * Purpose: Defines the Class GuideMainView
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid ee031133-1f35-4489-9045-e2f89f44b663 */
+public class GuideMainView extends State {
+   /** @pdOid 1a40dcb0-f8f5-4c80-aa7d-e426bdae84f2 */
+   public int do() {
+      // TODO: implement
+      return 0;
+   }
+   
+   /** @pdOid e78dbac8-9b58-4c33-a39a-a170d33518e8 */
+   public int entry() {
+      // TODO: implement
+      return 0;
+   }
+   
+   /** @pdOid e10e66f3-3baf-4b10-b1d2-8c51efc9e4f6 */
+   public int exit() {
+      // TODO: implement
+      return 0;
+   }
+
+}
---
+++ TourGuide/src/state/TourInfo.java
@@ -0,0 +1,29 @@
+/***********************************************************************
+ * Module:  TourInfo.java
+ * Author:  Nikola
+ * Purpose: Defines the Class TourInfo
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid c2c6fe9d-c824-4e2d-a46d-31f145bcd1fe */
+public class TourInfo extends State {
+   /** @pdOid 91453c49-e9d6-4557-a3a3-c0471eb72559 */
+   public int do() {
+      // TODO: implement
+      return 0;
+   }
+   
+   /** @pdOid d5bc1ec4-4739-4f35-97a2-1725c7ba2fbb */
+   public int entry() {
+      // TODO: implement
+      return 0;
+   }
+   
+   /** @pdOid 32f2a7aa-223e-40f0-b28c-bc9942b5a1bf */
+   public int exit() {
+      // TODO: implement
+      return 0;
+   }
+
+}
