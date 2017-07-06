dacf93c20096a6c4dd3c57ae4fbe7176e1bc7243
motomaniac96 <tomic.nikola.uns@gmail.com>
2017-07-06 23:58:54 +0200

    problem fixing

---
+++ TourGuide/src/state/LogIn.java
@@ -0,0 +1,29 @@
+/***********************************************************************
+ * Module:  LogIn.java
+ * Author:  Nikola
+ * Purpose: Defines the Class LogIn
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid cb8c36fa-20ef-49e2-8e13-dc2e02975c53 */
+public class LogIn extends State {
+   /** @pdOid 9361ebe5-ac6e-44e6-beeb-d22aa76c2126 */
+   public int do() {
+      // TODO: implement
+      return 0;
+   }
+   
+   /** @pdOid 8af20a39-384b-41de-a8ef-f4ba81747b31 */
+   public int entry() {
+      // TODO: implement
+      return 0;
+   }
+   
+   /** @pdOid f95d1d59-ab4b-4ddb-a6d6-3a25a40287e8 */
+   public int exit() {
+      // TODO: implement
+      return 0;
+   }
+
+}
