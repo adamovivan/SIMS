4801e06c0768134a9a2f8ab332dbacabcb107a9c
motomaniac96 <motomaniac96@users.noreply.github.com>
2017-07-06 23:31:18 +0200

    Delete Search.java

--- TourGuide/src/state/Search.java
+++
@@ -1,9 +0,0 @@
-/***********************************************************************
- * Module:  Search.java
- * Author:  PC
- * Purpose: Defines the Class Search
- ***********************************************************************/
-package state;
-
-public class Search extends State {
-}
