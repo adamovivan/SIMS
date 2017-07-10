f7bd04feedecee1bfa12ea3dbaf3b70cf75b3810
motomaniac96 <tomic.nikola.uns@gmail.com>
2017-07-10 23:10:24 +0200

    login param'd

--- TourGuide/src/controller/LogInController.java
+++ TourGuide/src/controller/LogInController.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import role.Person;
